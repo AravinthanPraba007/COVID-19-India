115762be641f1ec1913e345190319bb51727cbaa
Aravinthan Praba <799532>
2020-06-03 17:58:39 +0530

    Removed string builder and used string

--- src/main/java/com/aravinthan/covid19/service/NumberFormatHelperService.java
+++ src/main/java/com/aravinthan/covid19/service/NumberFormatHelperService.java
@@ -1,28 +1,38 @@
 package com.aravinthan.covid19.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NumberFormatHelperService {
 
-	public static String readableNumberFormat(String value){
+	public String readableNumberFormat(String value){
+//		System.out.println("value entered is -->"+value);
+		if(value.equals("0"))
+			return value;
         value=value.replace(",","");
         char lastDigit=value.charAt(value.length()-1);
-        StringBuilder result = new StringBuilder();
+//        StringBuilder result = new StringBuilder();
+        String result="";
         int len = value.length()-1;
         int nDigits = 0;
 
         for (int i = len - 1; i >= 0; i--)
         {
-            result.insert(0,value.charAt(i));
-            // result = value.charAt(i) + result;
+//            result.insert(0,value.charAt(i));
+             result = value.charAt(i) + result;
             nDigits++;
             if (((nDigits % 2) == 0) && (i > 0))
             {
-                result.insert(0,",");
-                // result = "," + result;
+//                result.insert(0,",");
+                 result = "," + result;
             }
         }
-        result.append(lastDigit);
-        // return (result+lastDigit);
-        value=result.toString();
-        return value;
+//        result.append(lastDigit);
+//        System.out.println("return value-->"+result+lastDigit);
+         return (result+lastDigit);
+//        value=result.toString();
+//        result.setLength(0);
+//        System.out.println("value returned -->"+result);
+//        return value;
     }
 }
