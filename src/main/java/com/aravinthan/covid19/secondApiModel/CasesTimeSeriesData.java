808033d9826a5b2ee9d160c1860e2976c3daf3c6
Aravinthan Praba <799532>
2020-06-03 16:39:38 +0530

    Converted the input value type to string

--- src/main/java/com/aravinthan/covid19/secondApiModel/CasesTimeSeriesData.java
+++ src/main/java/com/aravinthan/covid19/secondApiModel/CasesTimeSeriesData.java
@@ -7,29 +7,29 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Component
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CasesTimeSeriesData {
-	Integer dailyconfirmed;
-	Integer dailydeceased;
-	Integer dailyrecovered;
+	String dailyconfirmed;
+	String dailydeceased;
+	String dailyrecovered;
 	String date;
-	Integer totalconfirmed;
-	Integer totaldeceased;
-	Integer totalrecovered;
-	public Integer getDailyconfirmed() {
+	String totalconfirmed;
+	String totaldeceased;
+	String totalrecovered;
+	public String getDailyconfirmed() {
 		return dailyconfirmed;
 	}
-	public void setDailyconfirmed(Integer dailyconfirmed) {
+	public void setDailyconfirmed(String dailyconfirmed) {
 		this.dailyconfirmed = dailyconfirmed;
 	}
-	public Integer getDailydeceased() {
+	public String getDailydeceased() {
 		return dailydeceased;
 	}
-	public void setDailydeceased(Integer dailydeceased) {
+	public void setDailydeceased(String dailydeceased) {
 		this.dailydeceased = dailydeceased;
 	}
-	public Integer getDailyrecovered() {
+	public String getDailyrecovered() {
 		return dailyrecovered;
 	}
-	public void setDailyrecovered(Integer dailyrecovered) {
+	public void setDailyrecovered(String dailyrecovered) {
 		this.dailyrecovered = dailyrecovered;
 	}
 	public String getDate() {
@@ -38,22 +38,22 @@ public class CasesTimeSeriesData {
 	public void setDate(String date) {
 		this.date = date;
 	}
-	public Integer getTotalconfirmed() {
+	public String getTotalconfirmed() {
 		return totalconfirmed;
 	}
-	public void setTotalconfirmed(Integer totalconfirmed) {
+	public void setTotalconfirmed(String totalconfirmed) {
 		this.totalconfirmed = totalconfirmed;
 	}
-	public Integer getTotaldeceased() {
+	public String getTotaldeceased() {
 		return totaldeceased;
 	}
-	public void setTotaldeceased(Integer totaldeceased) {
+	public void setTotaldeceased(String totaldeceased) {
 		this.totaldeceased = totaldeceased;
 	}
-	public Integer getTotalrecovered() {
+	public String getTotalrecovered() {
 		return totalrecovered;
 	}
-	public void setTotalrecovered(Integer totalrecovered) {
+	public void setTotalrecovered(String totalrecovered) {
 		this.totalrecovered = totalrecovered;
 	}
 	public CasesTimeSeriesData() {
--- src/main/java/com/aravinthan/covid19/secondApiModel/StateWiseData.java
+++ src/main/java/com/aravinthan/covid19/secondApiModel/StateWiseData.java
@@ -8,50 +8,50 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class StateWiseData {
 
-	Integer active;
-	Integer confirmed;
-	Integer deaths;
-	Integer deltaconfirmed;
-	Integer deltadeaths;
-	Integer deltarecovered;
+	String active;
+	String confirmed;
+	String deaths;
+	String deltaconfirmed;
+	String deltadeaths;
+	String deltarecovered;
 	String lastupdatedtime;
-	Integer recovered;
+	String recovered;
 	String state;
 	String statecode;
-	public Integer getActive() {
+	public String getActive() {
 		return active;
 	}
-	public void setActive(Integer active) {
+	public void setActive(String active) {
 		this.active = active;
 	}
-	public Integer getConfirmed() {
+	public String getConfirmed() {
 		return confirmed;
 	}
-	public void setConfirmed(Integer confirmed) {
+	public void setConfirmed(String confirmed) {
 		this.confirmed = confirmed;
 	}
-	public Integer getDeaths() {
+	public String getDeaths() {
 		return deaths;
 	}
-	public void setDeaths(Integer deaths) {
+	public void setDeaths(String deaths) {
 		this.deaths = deaths;
 	}
-	public Integer getDeltaconfirmed() {
+	public String getDeltaconfirmed() {
 		return deltaconfirmed;
 	}
-	public void setDeltaconfirmed(Integer deltaconfirmed) {
+	public void setDeltaconfirmed(String deltaconfirmed) {
 		this.deltaconfirmed = deltaconfirmed;
 	}
-	public Integer getDeltadeaths() {
+	public String getDeltadeaths() {
 		return deltadeaths;
 	}
-	public void setDeltadeaths(Integer deltadeaths) {
+	public void setDeltadeaths(String deltadeaths) {
 		this.deltadeaths = deltadeaths;
 	}
-	public Integer getDeltarecovered() {
+	public String getDeltarecovered() {
 		return deltarecovered;
 	}
-	public void setDeltarecovered(Integer deltarecovered) {
+	public void setDeltarecovered(String deltarecovered) {
 		this.deltarecovered = deltarecovered;
 	}
 	public String getLastupdatedtime() {
@@ -60,10 +60,10 @@ public class StateWiseData {
 	public void setLastupdatedtime(String lastupdatedtime) {
 		this.lastupdatedtime = lastupdatedtime;
 	}
-	public Integer getRecovered() {
+	public String getRecovered() {
 		return recovered;
 	}
-	public void setRecovered(Integer recovered) {
+	public void setRecovered(String recovered) {
 		this.recovered = recovered;
 	}
 	public String getState() {
